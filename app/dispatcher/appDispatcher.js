4b5def69b2128a31c8343ac4cb4d3c45056f254d
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-03-25 17:22:25 -0600

    schema and this bug

--- app/dispatcher/appDispatcher.js
+++ app/dispatcher/appDispatcher.js
@@ -1,7 +1,7 @@
 var Dispatcher = require('flux').Dispatcher;
 var AppDispatcher = new Dispatcher();
 
-AppDispatcher.handleAction = (action) => {
+AppDispatcher.handleAction = function(action){
   this.dispatch({
     source: 'VIEW_ACTION',
     action: action
--- firebaseSchema.js
+++ firebaseSchema.js
@@ -1,19 +1,32 @@
 users
-  - id
-    - email
-    - name
-    - token
+  - id (tm*gmail*com)
+    - email (tylermcginnis33@gmail.com)
+    - name (Tyler McGinnis)
+    - token (XJDUY8ESX)
     - classes
-      - classid
-        - name (physics-tm*gmail*com)
+      - classid (physics-tm*gmail*com)
+        - isOwner (true)
+        - isMentor (false)
+        - isStudent (false)
+
 classes
-  - id (userid)
-    - name (physics-tm*gmail*com)
+  - id (physics-tm*gmail*com)
+    - name (Physics)
+    - mentors
+      - mentorid (jacob*devmounta*in)
     - students
-      - studentid
-        - name
+      - studentid (tm*gmail*com)
+        - name (Tyler McGinnis)
+        - email (tylermcginnis33@gmail.com)
 
 queue
   - classid (physics-tm*gmail*com)
-    - all students in queue (hash)
+    - student in queue (XEGS#SEGE)
+      - time entered (10139443)
+      - name || anonymous (Tyler McGinnis)
+      - question (Can you chain promises?)
 
+studentStatus
+  - classid (physics-tm*gmail*com)
+    - studentid (tm*gmail*com)
+      - status (84)
