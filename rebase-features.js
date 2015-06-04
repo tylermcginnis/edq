a6fb0484a45c7d7825c8ca359480e46d46728734
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-06-04 11:23:44 -0600

    Add rebase.js

---
+++ rebase-features.js
@@ -0,0 +1,5 @@
+- Have individual components subscribe to Firebase keys (listenTo)
+- AsObject / AsObject (bindToState)
+- Two way Binding
+- Unbind on ComponentWillUnmount
+- Way to Order Data Response
